f1deb561c0fc040155cc50cbbb08b7615a282c56
kim7751 <kiyoung0751@naver.com>
2021-04-25 16:18:48 +0900

    chapter 02

--- ch 02/ch 02.R
+++ ch 02/ch 02.R
@@ -21,7 +21,7 @@ head(iris, 10)
 tail(iris)
 plot(iris)
 
-# 두 속성의 상관 관계
+# 두 속성의 상관 관계 (col = 색깔로 종 구분 , pch = 점 표현 방식)
 plot(iris$Petal.Length, iris$Petal.Width, col=iris$Species, pch=18)
 
 
@@ -32,9 +32,14 @@ str(tips)
 # 요약 통계
 summary(tips)
 
-# ggplot2 그림 그려보기
-tips %>% ggplot(aes(size))+geom_histogram()
-tips %>% ggplot(aes(total_bill, tip))+geom_point()
-tips %>% ggplot(aes(total_bill, tip))+geom_point(aes(col=day))
+# ggplot2 그림 그려보기 (처음 ggplot 으로 축을 설정하고 + 그래프를 설정해 표현함)
+tips %>% ggplot(aes(size))+geom_histogram() #aes는 축을 설정함 / 히스토그램은 축 하나만 설정가능 다른 하나는 "카운트"
+tips %>% ggplot(aes(total_bill, tip))+geom_point() # 포인트는 양 축을 모두 정의 해줘야 함.
+tips %>% ggplot(aes(total_bill, tip))+geom_point(aes(col=day)) # 날짜별 컬러를 더해 시각화 효과 부각
 tips %>% ggplot(aes(total_bill, tip))+geom_point(aes(col=day,pch=sex),size=3)
 tips %>% ggplot(aes(total_bill, tip))+geom_point(aes(col=day,pch=time),size=3)                             
+
+# %>% 의 뜻 과 용법 예시
+
+y <- h(g(f(x))) # 괄호가 너무 많아서 복잡해 보임
+x %>% f() %>% g() %>% h() <- y # %>%를 사용해 괄호가 많아  헷갈리는 것을 방지
--- ch 03/ch 03-1.R
+++ ch 03/ch 03-1.R
@@ -7,12 +7,12 @@ y <- 2
 z <- x + y
 z
 
-# swapping
+# swapping (하나 변수를 만들어 x 값을 보관 / y 값을 비어있는 x에 이동 / 비어있는 y에 x값이 보관 된 변수 대입)
 temp <- x
 x <- y
 y <- temp
 
-# 변수의 타입
+# 변수의 타입 (character = 문자형 / "",'' 따옴표로 묶으면 문자형이 됨)
 typeof(x)
 a <- 'string'
 typeof(a)
@@ -21,16 +21,26 @@ typeof(b)
 c <- '한글'
 typeof(c)
 
-# 실수
+# 실수(Numeric 수치형 -> double 실수형)
 x <- 5
 y <- 2
-x / y
+typeof(x / y)
 
-# 복소수
+# 정수(Numeric 수치형 -> Int 정수형 / L은 완전한 정수를 표현함)
+x <- 5L
+y <- 2L
+typeof(x)
+
+#데이터 타입의 우선 순위(문자형이 가장 높고, 수치형, 논리형이 가장 낮다)
+
+character > numeric > logical
+
+# 복소수(Complex 복소수형 a + bi 형태)
 xi <- 1 + 2i
 yi <- 1 - 2i
 xi + yi
 xi * yi
+typeof(xi + yi)
 
 # 범주형
 blood_type = factor(c('A', 'B','O','AB'))
@@ -42,12 +52,12 @@ FALSE
 T
 F
 
-
+# 양의 무한대 / 음의 무한대 / NaN 수학적으로 계산 불가능
 xinf = Inf
 yinf = -Inf
-xinf / yinf
+xinf / yinf 
 
-# 데이터형 확인 함수
+# 데이터형 확인 함수(is. ??? --> '괄호안에 값이 ??? 형태 맞아?' 라는 뜻 )
 class(x) # R 객체지향 관점
 typeof(x) # R 언어자체 관점
 is.integer(x)
@@ -56,12 +66,12 @@ is.complex(xi)
 is.character(c)
 is.na(xinf/yinf)
 
-# 데이터형 변환 함수
+# 데이터형 변환 함수(as. ??? -> '괄호 안에 값 ??? 형태로 바꿀 꺼야' 하는 뜻 )
 is.integer(as.integer(x))
 is.factor(as.factor(c))
 
-# 산술연산자(+,-*,^,/)
-5 ^ 2
+# 산술연산자(+,-,*,^,/)
+5 ^ 2 #거듭제곱
 4 ^ (1/2)
 x %% y # 나머지
 x %/% y # 몫
@@ -74,16 +84,16 @@ x >= y
 x == y
 x != y
 
-# 논리 연산자
-!T
+# 논리 연산자(! 는 부정)
+!T 
 !F
-x | y   #OR
-x & y   #AND
+x | y   #OR  숫자 중 하나라도 1 포함 되면 T / 벡터 개별 원소끼리  비교 1 포함 T 
+x & y   #AND 숫자 중 하나라도 0 포함 되면 F / 벡터 개별 원소끼리  비교 0 포함 F / 비교 연산자와 쓰이면 교집합으로 쓰임 
 x || y
 a <- c(F, F, T, T)
 b <- c(F, T, F, T)
 a | b # element-wise OR 
-a || b # 첫번쩨 엘리먼트의 논리합
+a || b # 첫번쩨 엘리먼트만의 논리합
 a & b
-a && b
-2^-3-5**1/2>2 
+a && b # 첫번쩨 엘리먼트만의 논리합
+2 ^ -3 -5 ** 1/2 >2 # '**' 거듭제곱
