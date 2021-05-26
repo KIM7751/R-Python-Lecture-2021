c45faf22bad3dfa4d58f938e41ac7228160519ec
kiyoung kim <kiyoung0751@naver.com>
2021-05-26 17:04:44 +0900

    Python 5/26

---
+++ 2.Python/mod1.py
@@ -0,0 +1,6 @@
+# P 207 모듈 외부 라이브러리 만들기
+def add_mod1(a,b):
+    return a+b
+
+def sub_mod1(a,b):
+    return a-b
---
+++ 2.Python/mod2.py
@@ -0,0 +1,8 @@
+PI = 3.14
+
+def add_mod2(a, b):
+    return a+b
+
+class Math_Mod2:
+    def solve(self, r):
+        return PI * (r**2)
